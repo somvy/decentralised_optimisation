64273dc0c1fe425c484ef3f4ab82e9fd0091084b
Alexey Dontsov <admin@admins-MacBook-Pro.local>
2023-10-10 15:39:29 +0300

    feat: first commit

---
+++ opt/adom.py
@@ -0,0 +1,9 @@
+from base import BaseNode
+
+
+class AdomNode(BaseNode):
+    def __init__(self, fun, x):
+        super().__init__(fun, x)
+
+    def step(self):
+        pass
---
+++ opt/proxnsadom.py
@@ -0,0 +1,10 @@
+from base import BaseNode
+
+
+class ProxNSAdomNode(BaseNode):
+    def __init__(self, fun, x):
+        super().__init__(fun, x)
+
+    def step(self):
+        pass
+
