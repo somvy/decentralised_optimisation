a781584a1a78ca3f2c70fe672b2177d28f62a3c0
Alexey Dontsov <admin@admins-MacBook-Pro.local>
2023-10-16 12:54:17 +0300

    test for mnist convergence

--- tests/test_dgd_nn.py
+++ tests/test_dgd_nn.py
@@ -23,10 +23,11 @@ def test_decentralized_gd_nn():
         oracle = MNISTMLP(Xy=subset, init_network=init_model)
         oracles.append(oracle)
 
-    method = DecentralizedGradientDescent(oracles=oracles, topology=topology, stepsize=1e-3, max_iter=1000)
+    method = DecentralizedGradientDescent(oracles=oracles, topology=topology, stepsize=4e-3, max_iter=40000)
     loss = sum([oracle() for oracle in oracles]) / len(oracles)
     method.run(log=True)
 
+    save_plot(method.logs, "dgd_nn.png")
     loss_after = sum([oracle() for oracle in oracles]) / len(oracles)
     print(f"losses: {loss_after} | {loss}")
     # assert loss_after < loss
--- tests/test_oracles.py
+++ tests/test_oracles.py
@@ -1,3 +1,23 @@
-
 from oracles.mnist_mlp_oracle import MNISTMLP, MLP
+from torchvision.datasets import MNIST
+from torchvision import transforms
+from utils import save_plot
 
+
+def test_nn():
+    init_model = MLP()
+    train_dataset = MNIST(".", download=True, transform=transforms.ToTensor(), train=True)
+    oracle = MNISTMLP(Xy=train_dataset, init_network=init_model)
+
+    iters = 100000
+    stepsize = 3e-4
+    log = []
+
+    for _ in range(iters):
+        x = oracle.get_params()
+        grads = oracle.grad()
+        new_params = [param - stepsize * grad for param, grad in zip(x, grads)]
+        oracle.set_params(new_params)
+        log.append({"loss": oracle()})
+
+    save_plot(log, "nn.png")
