9c5790b411163fcfbd2f29915de6a882bc16109f
Feoktistov Dmitriy <feoctistov1580@yandex.ru>
2023-10-27 12:50:24 +0300

    correct zo-sadom

--- methods/ZOSADOM.py
+++ methods/ZOSADOM.py
@@ -5,6 +5,7 @@ from torch import Tensor
 from copy import deepcopy
 import numpy as np
 from methods.base import BaseDecentralizedMethod
+from tqdm.auto import trange
 
 
 class ZOSADOM(BaseDecentralizedMethod):
@@ -12,23 +13,25 @@ class ZOSADOM(BaseDecentralizedMethod):
         super().__init__(oracles, topology)
         self.max_iter = max_iter
 
-        self.x_f_all: list[list[Tensor]] = [oracle.get_params() for oracle in self.oracles]
+        self.x_all: list[list[Tensor]] = [oracle.get_params() for oracle in self.oracles]
         # initialize z in L orthogonal
         # oracles x layers x params
-        self.z_all = [[torch.zeros_like(param) for param in oracles] for oracles in self.x_f_all]
-        self.y_f_all = deepcopy(self.x_f_all)
-        self.y_all = deepcopy(self.x_f_all)
+        self.z_all = [[torch.zeros_like(tmp) for tmp in el] for el in self.x_all]
+        self.y_all = deepcopy(self.x_all)
+        self.m_all = deepcopy(self.z_all)
+        self.x_f_all = deepcopy(self.x_all)
+        self.y_f_all = deepcopy(self.x_all)
         self.z_f_all = deepcopy(self.z_all)
-        self.m_all = deepcopy(self.x_f_all)
         self.__calculate_params()
     
     def __calculate_params(self):
-        L = np.sqrt(self.oracles[0].dimension) * self.oracles[0].lipschitz / self.oracles[0].gamma
+        L = 1#np.sqrt(self.oracles[0].dimension) * self.oracles[0].lipschitz / self.oracles[0].gamma
         mu = self.oracles[0].regularization
         chi = self.topology.chi
+        #print(chi)
         self.params = {}
         self.params['zeta'] = 0.5
-        self.params['beta'] = 1. / (2 * L)
+        self.params['beta'] = 20#1. / (2 * L)
         self.params['nu'] = mu / 2
         self.params['alpha'] = mu / 4
         self.params['tau2'] = np.sqrt(mu / L)
@@ -39,18 +42,16 @@ class ZOSADOM(BaseDecentralizedMethod):
         self.params['sigma1'] = 1. / (1 / self.params['sigma2'] + 0.5)
         self.params['kappa'] = self.params['nu'] / (14 * self.params['sigma2'] * chi * chi)
         self.params['teta'] = self.params['nu'] / (4 * self.params['sigma2'])
-        
-
-    def __get_grad_in_x_g(self) -> list[list[Tensor]]:
-        x = [oracle.get_params() for oracle in self.oracles] # node x layer x param
-        layers_count = len(x[0])
+    
+    def compute_grad_in_x_g(self):
+        x = self.x_all # node x layer x param
         x_f = self.x_f_all
-        # calculate gradients in x_g
+        layers_count = len(x[0])
         for oracle_num, oracle in enumerate(self.oracles):
             oracle.set_params(
-                [self.params["tau1"] * x[oracle_num][layer_num] + (1 - self.params["tau1"]) * x_f[oracle_num][
-                    layer_num].reshape(x[oracle_num][layer_num].shape)
-                 for layer_num in range(layers_count)]
+                [self.params['tau1'] * x[oracle_num][layer_num] + 
+                 (1 - self.params['tau1']) * x_f[oracle_num][layer_num]
+                     for layer_num in range(layers_count)]
             )
         gradients = [oracle.grad() for oracle in self.oracles]
         # set params back
@@ -59,81 +60,83 @@ class ZOSADOM(BaseDecentralizedMethod):
                 x[oracle_num]
             )
         return gradients
-
+    
+        
     def step(self):
         assert self.topology.matrix_type.startswith("gossip"), "works with gossip matrices only yet"
-
-        x_all: list[list[Tensor]] = [oracle.get_params() for oracle in
-                                     self.oracles]  # node x layer x param
-        gossip_matrix: Tensor = torch.tensor(next(self.topology), dtype=x_all[0][0].dtype)
-        layers_count = len(x_all[0])
+        gossip_matrix: Tensor = torch.tensor(next(self.topology), dtype=self.x_all[0][0].dtype)
+        layers_count = len(self.x_all[0])
         oracles_counter = range(len(self.oracles))
-        grad_all = self.__get_grad_in_x_g()  # node x layer x param
         updated_params_by_layer = {"x": [], "y": [], "z": [], "m": [], "x_f": [], "y_f": [], "z_f": []}
-
+        grad_all = self.compute_grad_in_x_g()
         # accumulate params and grads by layer
         for layer_num in range(layers_count):
-            x = torch.stack([x_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            y = torch.stack([self.y_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            z = torch.stack([self.z_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            x_f = torch.stack([self.x_f_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            y_f = torch.stack([self.y_f_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            z_f = torch.stack([self.z_f_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            m = torch.stack([x_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
-            grad = torch.stack([grad_all[oracle_num][layer_num] for oracle_num in oracles_counter]).squeeze()
+            x: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.x_all])
+            y: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.y_all])
+            z: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.z_all])
+            x_f: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.x_f_all])
+            y_f: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.y_f_all])
+            z_f: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.z_f_all])
+            m: tensor = torch.stack([oracle_params[layer_num] for oracle_params in self.m_all])
+            grad: tensor = torch.stack([oracle_params[layer_num] for oracle_params in grad_all])
             x_g = self.params['tau1'] * x + (1 - self.params['tau1']) * x_f
             y_g = self.params['sigma1'] * y + (1 - self.params['sigma1']) * y_f
             z_g = self.params['sigma1'] * z + (1 - self.params['sigma1']) * z_f
-
-            const1 = y + self.params['teta'] * self.params['beta'] * (grad - self.params['nu'] * x_g)
-            const1 -= self.params['teta'] / self.params['nu'] * (y_g + z_g)
-            const2 = x + self.params['eta'] * self.params['alpha'] * x_g
-            const2 -= self.params['eta'] * (grad - self.params['nu'] * x_g)
-
-            x_new = const2 + self.params['eta'] / (1 + self.params['teta'] * self.params['beta']) * const1
+            # grad = torch.zeros_like(grad)
+            
+            x_new = grad - self.params['nu'] * x_g - y + self.params['teta'] / self.params['nu'] * (y_g + z_g)
+            x_new *= self.params['eta'] / (1 + self.params['teta'] * self.params['beta'])
+            x_new = x + self.params['eta'] * self.params['alpha'] * x_g - x_new
             denum = 1 + self.params['eta'] * self.params['alpha']
-            denum += self.params['teta'] * self.params['eta'] / (1 + self.params['teta'] * self.params['beta'])
+            denum += self.params['eta'] * self.params['teta'] / (1 + self.params['teta'] * self.params['beta'])
             x_new /= denum
-            y_new = const1 - self.params['teta'] * x_new
+            
+            y_new = self.params['teta'] * ((y_g + z_g) / self.params['nu'] + x)
+            y_new = y + self.params['teta'] * self.params['beta'] * (grad - self.params['nu'] * x_g) - y_new
             y_new /= 1 + self.params['teta'] * self.params['beta']
             
-            step = gossip_matrix @ (self.params['kappa'] / self.params['nu'] * (y_g + z_g) + m)
+            x_f_new = x_g + self.params['tau2'] * (x_new - x)
+            y_f_new = y_g + self.params['sigma2'] * (y_new - y)
+            delta_m = self.params['kappa'] * (y_g + z_g) / self.params['nu'] + m
+            delta_z = y_g + z_g
+            
+            n, *d = delta_m.shape
+            delta_m = torch.matmul(gossip_matrix, delta_m.view(n, -1)).view(n, *d)
+    
+            n, *d = delta_z.shape
+            delta_z = torch.matmul(gossip_matrix, delta_z.view(n, -1)).view(n, *d)
+            
+            z_new = z + self.params['kappa'] * self.params['pi'] * (z_g - z) - delta_m
+            m_new = m + self.params['kappa'] / self.params['nu'] * (y_g + z_g) - delta_m
+            
+            z_f_new = z_g - self.params['zeta'] * delta_z
+            updated_params_by_layer['x'].append(x_new)
+            updated_params_by_layer['y'].append(y_new)
+            updated_params_by_layer['z'].append(z_new)
+            updated_params_by_layer['m'].append(m_new)
+            updated_params_by_layer['x_f'].append(x_f_new)
+            updated_params_by_layer['y_f'].append(y_f_new)
+            updated_params_by_layer['z_f'].append(z_f_new)
+            
 
-            z_new = z + self.params['kappa'] * self.params['pi'] * (z_g - z)
-            z_new -= step
-            m_new = self.params['kappa'] / self.params['nu'] * (y_g + z_g) + m - step
-
-            x_f = x_g + self.params['tau2'] * (x_new - x)
-            y_f = y_g + self.params['sigma2'] * (y_new - y)
-            z_f = z_g - self.params['zeta'] * (gossip_matrix @ y_g + z_g)
-
-            updated_params_by_layer["x"].append(x_new)
-            updated_params_by_layer["y"].append(y_new)
-            updated_params_by_layer["z"].append(z_new)
-            updated_params_by_layer["m"].append(m_new)
-            updated_params_by_layer["x_f"].append(x_f)
-            updated_params_by_layer["y_f"].append(y_f)
-            updated_params_by_layer["z_f"].append(z_f)
-
+        self.x_all = self.change_dim_layer_to_oracle(updated_params_by_layer["x"])
+        self.y_all = self.change_dim_layer_to_oracle(updated_params_by_layer["y"])
+        self.z_all = self.change_dim_layer_to_oracle(updated_params_by_layer["z"])
+        self.m_all = self.change_dim_layer_to_oracle(updated_params_by_layer["m"])
         self.x_f_all = self.change_dim_layer_to_oracle(updated_params_by_layer["x_f"])
-
         self.y_f_all = self.change_dim_layer_to_oracle(updated_params_by_layer["y_f"])
         self.z_f_all = self.change_dim_layer_to_oracle(updated_params_by_layer["z_f"])
-        self.z_all = self.change_dim_layer_to_oracle(updated_params_by_layer["z"])
-        self.y_all = self.change_dim_layer_to_oracle(updated_params_by_layer["y"])
-        self.m_all = self.change_dim_layer_to_oracle(updated_params_by_layer["m"])
-        
         # set x_new in oracles
         for oracle_num, oracle in enumerate(self.oracles):
             oracle.set_params(
-                [updated_params_by_layer["x"][layer_num][oracle_num].reshape(x_all[oracle_num][layer_num].shape)
-                         for layer_num in range(layers_count)]
+                [self.x_all[oracle_num][layer_num] for layer_num in range(layers_count)]
             )
 
-    def run(self, log: bool = False):
+    def run(self, log: bool = False, disable_tqdm=True):
+        loop = trange(1, self.max_iter + 1, disable=disable_tqdm)
         if log:
             self.logs.append(self.log())
-        for _ in range(self.max_iter):
+        for i in loop:
             self.step()
             if log:
                 self.logs.append(self.log())
